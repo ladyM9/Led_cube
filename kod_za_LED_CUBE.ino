a6e212ec61f86c4600876480dbf21301c70c9b9d
ladyM9 <78236758+ladyM9@users.noreply.github.com>
2022-08-18 00:26:06 +0200

    Prvi kod za led cube, neopixel example ustvari

---
+++ kod_za_LED_CUBE.ino
@@ -0,0 +1,32 @@
+#include <Adafruit_NeoPixel.h>
+#define LED_PIN     PB0
+#define LED_COUNT  64
+#define BRIGHTNESS 5 // Set BRIGHTNESS to about 1/5 (max = 255)
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+void setup() {
+  // put your setup code here, to run once:
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(BRIGHTNESS);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  colorWipe(strip.Color(255,   0,   0)     , 50); // Red
+  colorWipe(strip.Color(  0, 255,   0)     , 50); // Green
+  colorWipe(strip.Color(  0,   0, 255)     , 50); // Blue
+
+}
+
+void colorWipe(uint32_t color, int wait) {
+  for (int i = 0; i < strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
