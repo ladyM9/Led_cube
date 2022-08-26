6222e680b1c4f00461c649ceba6d8dab3099bdfa
ladyM9 <78236758+ladyM9@users.noreply.github.com>
2022-08-26 18:23:15 +0200

    Dodano više različitih oblika koje se prikazuju pojedinačno na led kocki

--- PROBA.ino
+++ PROBA.ino
@@ -2,11 +2,12 @@
 //#include <Adafruit_GFX.h>
 LEDCube myCube;
 //void drawRect(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h, uint16_t color);
-uint16_t x0[] = {0, 1, 2, 3, 4, 5, 6};
-uint16_t color[] = {12555, 123, 29857, 35214, 48975, 60000};
+uint16_t x0[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+uint16_t y[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+uint16_t color[] = {12555, 123, 29857, 35214, 48975, 60000, 18000, 7841};
 unsigned long previousMillis = 0;
 const long interval = 1000;
-const long interval2 = 1000;
+const long interval2 = 10000;
 void setup() {
   // put your setup code here, to run once:
   //  uint16_t x0 = 1;
@@ -18,17 +19,17 @@ void setup() {
   //  uint16_t h = 5 ;
 
   myCube.begin();
- // myCube.clear();
+  // myCube.clear();
   // myCube.drawRect( 0, 2, 4, 4, 12555);
   //myCube.drawChar(0, 0, 89, 65535, 0, 1);
   // myCube.display();
 }
-
+int i = 0;
 void loop() {
   unsigned long currentMillis = millis();
 
   // put your main code here, to run repeatedly:
-  if (currentMillis  > interval) {
+  if (currentMillis  < interval) {
     previousMillis = currentMillis;
     // save the last time you blinked the LED
     for (int i = 0; i < 6 ; i++) {
@@ -36,51 +37,47 @@ void loop() {
       myCube.drawRect( x0[i], 2, 4, 4, color[i]);
       //myCube.drawChar(0, 0, 89, 65535, 0, 1);
       myCube.display();
+      delay(250);
     }
-  } else {
-    myCube.setDepth(1);
-    myCube.drawPixel(1, 0, 12535);
-    myCube.setDepth(1);
-    myCube.drawPixel(1, 2, 32568);
-    myCube.setDepth(1);
-    myCube.drawPixel(1, 1 , 65535);
-    myCube.setDepth(1);
-    myCube.drawPixel(1, 3 , 52138);
-    myCube.setDepth(1);
-    myCube.drawPixel(1, 4 , 65535);
-    myCube.setDepth(1);
-    myCube.drawPixel(1, 5 , 789);
-    myCube.setDepth(1);
-    myCube.drawPixel(1, 6 , 65535);
-    myCube.setDepth(2);
-    myCube.drawPixel(1, 5 , 65535);
-    myCube.setDepth(2);
-    myCube.drawPixel(2, 4 , 65535);
-    myCube.setDepth(2);
-    myCube.drawPixel(3, 3 , 65535);
-    myCube.setDepth(2);
-    myCube.drawPixel(4, 2 , 65535);
-    myCube.setDepth(2);
-    myCube.drawPixel(5, 0 , 65535);
-
-    ///Desna linija slova N
-    myCube.setDepth(3);
-    myCube.drawPixel(6, 0 , 65535);
-    myCube.setDepth(3);
-    myCube.drawPixel(6, 1 , 65535);
-    myCube.setDepth(3);
-    myCube.drawPixel(6, 2 , 2800);
-    myCube.setDepth(3);
-    myCube.drawPixel(6, 3 , 5535);
-    myCube.setDepth(3);
-    myCube.drawPixel(6, 4 , 17535);
-    myCube.setDepth(3);
-    myCube.drawPixel(6, 5 , 65535);
-    myCube.setDepth(3);
-    myCube.drawPixel(6, 6 , 52535);
-    myCube.display();
-    myCube.clear();
   }
- // myCube.clear();
+  if (currentMillis > 2000 && currentMillis < 5000 ) {
+    myCube.clear();
+    myCube.setDepth(2);
+    myCube.fillCircle(2, 3, 2, 25874);
+    myCube.setDepth(4);
+    myCube.fillCircle(5, 3, 2, 45213);
+    myCube.display();
+  }
+  if ((currentMillis > 6000) && (currentMillis < 10000)) {
+    myCube.clear();
+    myCube.setCursor(1, 0);
+    myCube.setDepth(i % 8);
+    myCube.print((i++) % 9, DEC);
+    myCube.display();
+    delay(250);
+  }
+  if ((currentMillis > 10000) && (currentMillis < 12000)) {
+    myCube.clear();
+    myCube.setDepth(2);
+    myCube.drawLine(1, 1, 6, 5, 55879);
+    myCube.setDepth(5);
+    myCube.drawLine(1, 7, 1, 1, 22145);
+    myCube.display();
+  }
+  if ((currentMillis > 12000) && (currentMillis < 15000)) {
+    myCube.clear();
+    for (int i = 0; i < 8 ; i++) {
+      myCube.setDepth(i);
+      myCube.drawPixel(x0[i], y[i], color[i]);
+      myCube.display();
+      delay(250);
+    }
+    for (int i = 0; i > 8 ; i++) {
+      myCube.setDepth(i-1);
+      myCube.drawPixel(x0[i], y[i], color[i]);
+      myCube.display();
 
+    }
+    return;
+  }
 }
