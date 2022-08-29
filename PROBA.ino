db42b7737e56b29d42447d3f71b51151a7170ed9
ladyM9 <78236758+ladyM9@users.noreply.github.com>
2022-08-30 01:27:49 +0200

    Optimizacija timera, animacije se stalno mijenjaju na led kocki zbog pravilnog korištenja timera i intervala

--- PROBA.ino
+++ PROBA.ino
@@ -8,6 +8,10 @@ uint16_t color[] = {12555, 123, 29857, 35214, 48975, 60000, 18000, 7841};
 unsigned long previousMillis = 0;
 const long interval = 1000;
 const long interval2 = 10000;
+
+unsigned long change_Timer = 0;
+int pattern = 0;
+unsigned long TIMER_CHANGE_INTERVAL = 5000;
 void setup() {
   // put your setup code here, to run once:
   //  uint16_t x0 = 1;
@@ -26,58 +30,116 @@ void setup() {
 }
 int i = 0;
 void loop() {
-  unsigned long currentMillis = millis();
+  upadate_Animacije(pattern);  //pozivanje funkcije pattern
+  //unsigned long currentMillis = millis()
+  if ((unsigned long)(millis() - change_Timer) >= TIMER_CHANGE_INTERVAL)  ///ako je  razlika vremena otkad je upaljen mikroupravljač i vremena promjene vremena veća od promjene intervala učini sljedeće
+  {
+    change_Timer = millis();  //varijabli za promjenu timera priduži millis odnosno milisekunde odnosno kreni brojati
+    pattern++;  //pattern je ustvari _p iz funkcije za promjenu animacije, pomoću _p odnosno pattern odabire se case tj animacija
+    if (pattern > 4)  //ako su animacije odnosno caseovi došli do kraja postavi pattern na 0 i kreni ispočetka
+    {
+      pattern = 0;
+    }
+  }
 
-  // put your main code here, to run repeatedly:
-  if (currentMillis  < interval) {
-    previousMillis = currentMillis;
-    // save the last time you blinked the LED
-    for (int i = 0; i < 6 ; i++) {
-      myCube.setDepth(i);
-      myCube.drawRect( x0[i], 2, 4, 4, color[i]);
-      //myCube.drawChar(0, 0, 89, 65535, 0, 1);
+
+
+  return;
+}
+
+//  // put your main code here, to run repeatedly:
+// // if (currentMillis + previousMillis  < millis()  || millis() < previousMillis) {
+////    previousMillis = currentMillis;
+//    // save the last time you blinked the LED
+//    for (int i = 0; i < 6 ; i++) {
+//      myCube.setDepth(i);
+//      myCube.drawRect( x0[i], 2, 4, 4, color[i]);
+//      //myCube.drawChar(0, 0, 89, 65535, 0, 1);
+//      myCube.display();
+//      delay(250);
+//    }
+//  }
+//  if (currentMillis > 2000 && currentMillis < 5000 ) {
+//  myCube.clear();
+//    myCube.setDepth(2);
+//    myCube.fillCircle(2, 3, 2, 25874);
+//    delay(250);
+//    myCube.setDepth(4);
+//    myCube.fillCircle(5, 3, 2, 45213);
+//    myCube.display();
+//  }
+//  if ((currentMillis > 6000) && (currentMillis < 10000)) {
+//    myCube.clear();
+//    myCube.setCursor(1, 0);
+//    myCube.setDepth(i % 8);
+//    myCube.print((i++) % 9, DEC);
+//    myCube.display();
+//    delay(250);
+//  }
+//  if ((currentMillis > 10000) && (currentMillis < 12000)) {
+//    myCube.clear();
+//    myCube.setDepth(2);
+//    myCube.drawLine(1, 1, 6, 5, 55879);
+//    myCube.setDepth(5);
+//    myCube.drawLine(1, 7, 1, 1, 22145);
+//    myCube.display();
+//  }
+//  if ((currentMillis > 12000) && (currentMillis < 15000)) {
+//    myCube.clear();
+//    for (int i = 0; i < 8 ; i++) {
+//      myCube.setDepth(i);
+//      myCube.drawPixel(x0[i], y[i], color[i]);
+//      myCube.display();
+//      delay(250);
+//    }
+//    for (int i = 0; i > 8 ; i++) {
+//      myCube.setDepth(i - 1);
+//      myCube.drawPixel(x0[i], y[i], color[i]);
+//      myCube.display();
+//
+//    }
+
+
+
+
+void  upadate_Animacije(int _p)   ///funkcija pomoću koje odabirem animaciju koju želim da mi se prikazuje na led kocki
+{
+  switch (_p)
+  {
+    case 0:
+      myCube.clear();
+      for (int i = 0; i < 8 ; i++)
+      {
+        myCube.setDepth(i);
+        myCube.drawPixel(x0[i], y[i], color[i]);
+        myCube.display();
+        delay(250);
+      } break;
+    case 1:
+      myCube.clear();
+      myCube.setCursor(1, 0);
+      myCube.setDepth(i % 8);
+      myCube.print((i++) % 9, DEC);
       myCube.display();
       delay(250);
-    }
-  }
-  if (currentMillis > 2000 && currentMillis < 5000 ) {
-    myCube.clear();
-    myCube.setDepth(2);
-    myCube.fillCircle(2, 3, 2, 25874);
-    myCube.setDepth(4);
-    myCube.fillCircle(5, 3, 2, 45213);
-    myCube.display();
-  }
-  if ((currentMillis > 6000) && (currentMillis < 10000)) {
-    myCube.clear();
-    myCube.setCursor(1, 0);
-    myCube.setDepth(i % 8);
-    myCube.print((i++) % 9, DEC);
-    myCube.display();
-    delay(250);
-  }
-  if ((currentMillis > 10000) && (currentMillis < 12000)) {
-    myCube.clear();
-    myCube.setDepth(2);
-    myCube.drawLine(1, 1, 6, 5, 55879);
-    myCube.setDepth(5);
-    myCube.drawLine(1, 7, 1, 1, 22145);
-    myCube.display();
-  }
-  if ((currentMillis > 12000) && (currentMillis < 15000)) {
-    myCube.clear();
-    for (int i = 0; i < 8 ; i++) {
-      myCube.setDepth(i);
-      myCube.drawPixel(x0[i], y[i], color[i]);
+      break;
+    case 2:
+      myCube.clear();
+      myCube.setDepth(2);
+      myCube.drawLine(1, 1, 6, 5, 55879);
+      myCube.setDepth(5);
+      myCube.drawLine(1, 7, 1, 1, 22145);
       myCube.display();
-      delay(250);
-    }
-    for (int i = 0; i > 8 ; i++) {
-      myCube.setDepth(i-1);
-      myCube.drawPixel(x0[i], y[i], color[i]);
-      myCube.display();
-
-    }
-    return;
+      break;
+    case 3:
+      myCube.clear();
+      for (int i = 0; i < 6 ; i++)
+      {
+        myCube.setDepth(i);
+        myCube.drawRect( x0[i], 2, 4, 4, color[i]);
+        //myCube.drawChar(0, 0, 89, 65535, 0, 1);
+        myCube.display();
+        delay(250);
+      } break;
   }
 }
