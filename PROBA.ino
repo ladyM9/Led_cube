11824308f080ea4c138363b27e9de0b44576fa0e
ladyM9 <78236758+ladyM9@users.noreply.github.com>
2022-09-01 01:39:21 +0200

    Ispisivanje moguće na dvije plohe led kocke, dodane još dvije animacije

--- PROBA.ino
+++ PROBA.ino
@@ -4,7 +4,9 @@ LEDCube myCube;
 //void drawRect(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h, uint16_t color);
 uint16_t x0[] = {0, 1, 2, 3, 4, 5, 6};
 uint16_t y[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-char slova[5] = {'F','E','R','I','T'};
+char slova[5] = {'F', 'E', 'R', 'I', 'T'};
+char slova_2[8] = {'L', 'E', 'D', '_', 'C', 'U', 'B', 'E'};
+uint16_t linijax[5] = {0,2,4,6,8};
 uint16_t color[] = {12555, 123, 29857, 35214, 48975, 60000, 18000, 7841};
 unsigned long previousMillis = 0;
 const long interval = 1000;
@@ -37,7 +39,7 @@ void loop() {
   {
     change_Timer = millis();  //varijabli za promjenu timera priduži millis odnosno milisekunde odnosno kreni brojati
     pattern++;  //pattern je ustvari _p iz funkcije za promjenu animacije, pomoću _p odnosno pattern odabire se case tj animacija
-    if (pattern > 6)  //ako su animacije odnosno caseovi došli do kraja postavi pattern na 0 i kreni ispočetka
+    if (pattern > 10)  //ako su animacije odnosno caseovi došli do kraja postavi pattern na 0 i kreni ispočetka
     {
       pattern = 0;
     }
@@ -107,34 +109,48 @@ void  upadate_Animacije(int _p)   ///funkcija pomoću koje odabirem animaciju ko
 {
   switch (_p)
   {
-    case 0:
+    case 0:  //ispisivanje teksta LED_CUBE
       myCube.clear();
-      for (int i = 0; i < 5 ; i++)
+      for (int i = 0 ; i < 8 ; i++)
       {
-        myCube.setDepth(i);
-        myCube.drawPixel(x0[i], y[i], color[i]);
+        myCube.setCursor(0, 0);
+        myCube.setDepth(7);      //PODEŠAVANJE Z OSI
+        myCube.setPlane(1);       //PODEŠAVANJE STRANICE NA KOJOJ ŽELIM DA MI SE NEŠTO ISPISUJE NA LED KOCKI
+        myCube.setTextColor(65535, 0);
+        myCube.setTextSize(1);
+        myCube.print(slova_2[i]);
         myCube.display();
-        delay(250);
+        delay(500);
       }
-      for (int i = 8; i > 0 ; i--)
-      {
-        myCube.setDepth(8);
-        myCube.drawPixel(x0[i], y[i], color[i]);
-        myCube.display();
-        delay(250);
-      } break;
-    case 1:
+      break;
+
+    case 1:  //iscrtavanje krugova
+
       myCube.clear();
-      myCube.setCursor(1, 0);
-      myCube.setDepth(i % 8);
-      myCube.print((i++) % 9, DEC);
-      myCube.display();
-      delay(250);
+      for (int i = 0 ; i < 7 ; i++)
+      {
+        myCube.setPlane(0);
+        myCube.setDepth(0);
+        myCube.drawCircle(x0[i] , 3, 1, 30000);
+        //myCube.drawPixel_3(5, 0 , 1256);
+        myCube.display();
+        delay(500);
+      }
+      for (int i = 0 ; i < 7 ; i++)
+      {
+        myCube.setPlane(1);
+        myCube.setDepth(7);
+        myCube.drawCircle(x0[i] , 3, 1, 12589);
+        //myCube.drawPixel_3(5, 0 , 1256);
+        myCube.display();
+        delay(500);
+      }
       break;
     case 2:
       myCube.clear();
       for (int i = 8 ; i > 0 ; i--)
       {
+        myCube.setPlane(0);
         myCube.setDepth(3);
         myCube.drawPixel(3, y[i] , color[i]);
         myCube.setDepth(6);
@@ -143,15 +159,31 @@ void  upadate_Animacije(int _p)   ///funkcija pomoću koje odabirem animaciju ko
         myCube.drawPixel(1, y[i] + 1 , color[i]);
         myCube.setDepth(2);
         myCube.drawPixel(6, y[i] , color[i]);
+        myCube.setDepth(1);
+        myCube.drawPixel(0, y[i] , color[i]);
         myCube.display();
-        delay(500);
+        delay(200);
+      }
+      for (int i = 0 ; i < 8 ; i++)
+      {
+        myCube.setPlane(0);
+        myCube.setDepth(4);
+        myCube.drawPixel(0, y[i] , 22500);
+        myCube.setDepth(0);
+        myCube.drawPixel(8, y[i]  , 41000);
+        myCube.setDepth(7);
+        myCube.drawPixel(5, y[i]  , 5289);
+        myCube.display();
+        delay(200);
       }
       break;
 
     case 3:
       myCube.clear();
+      myCube.setPlane(0);
       for (int y = 8 ; y > 0 ; y--)
       {
+
         myCube.setDepth(3);
         myCube.drawCircle( 2, y, 1, 12458);
         delay(250);
@@ -160,7 +192,14 @@ void  upadate_Animacije(int _p)   ///funkcija pomoću koje odabirem animaciju ko
       for (int y = 0 ; y < 8 ; y++)
       {
         myCube.setDepth(1);
-        myCube.drawCircle( 5, y, 1, 32000);
+        myCube.drawCircle( 5, y, 1, 42158);
+        delay(250);
+        myCube.display();
+      }
+      for (int y = 0 ; y < 8 ; y++)
+      {
+        myCube.setDepth(7);
+        myCube.drawCircle( 3, y, 1, 20000);
         delay(250);
         myCube.display();
       }
@@ -180,13 +219,74 @@ void  upadate_Animacije(int _p)   ///funkcija pomoću koje odabirem animaciju ko
       for (int i = 0 ; i < 5; i++)
       {
         myCube.setCursor(0, 0);
+        myCube.setDepth(0);
         myCube.setTextColor(65535, 0);
         myCube.setTextSize(1);
-        myCube.print(i);
+        myCube.print(slova[i]);
         myCube.display();
-      }
-
+        delay(250);
+      } break;
+    case 6:
+      myCube.clear();
+      myCube.setCursor(1, 0);
+      myCube.setDepth(i % 8);
+      myCube.print((i++) % 9, DEC);
+      myCube.display();
+      delay(250);
       break;
 
+    case 7:
+      myCube.clear();
+      for (int i = 0; i < 8 ; i++)
+      {
+        myCube.setDepth(i);
+        myCube.drawPixel(x0[i], y[i], color[i]);
+        myCube.display();
+        delay(250);
+      }
+      for (int i = 8; i > 0 ; i--)
+      {
+        myCube.setDepth(8);
+        myCube.drawPixel(x0[i], y[i], color[i]);
+        myCube.display();
+        delay(250);
+      }
+      for (int i = 0; i < 8 ; i++)
+      {
+        myCube.setDepth(i);
+        myCube.drawPixel(x0[i], 0, color[i]);
+        myCube.display();
+        delay(250);
+      } break;
+    case 8:
+      myCube.clear();
+      for (int i = 0; i < 7 ; i++)
+      {
+        myCube.setDepth(i);
+        myCube.drawFastVLine(x0[i], y[i], 4, 4587);
+        myCube.display();
+        delay(250);
+
+      } break;
+    case 9:
+      myCube.clear();
+      for (int i = 0 ; i < 7 ; i++)
+      {
+        myCube.setDepth(i);
+        myCube.fillScreen(12589);
+        myCube.display();
+        delay(250);
+
+      } break;
+    case 10:
+      myCube.clear();
+      for(int i = 0 ; i < 7 ; i++)
+      { 
+      myCube.setDepth(i);
+      myCube.drawFastVLine(linijax[i], 0, 8, color[i]);
+      myCube.display();
+      delay(250);
+      }
+      break;
   }
 }
