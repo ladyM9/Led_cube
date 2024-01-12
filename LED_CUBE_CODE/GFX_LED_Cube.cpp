4c6d7979b2b0650784e1394534337993e446e249
ladyM9 <marinagolic12@gmail.com>
2024-01-12 17:56:13 +0100

    Deleted drawPixel_2, drawPixel_3 method, setDepht_2 and _3 method

--- LED_CUBE_CODE/GFX_LED_Cube.cpp
+++ LED_CUBE_CODE/GFX_LED_Cube.cpp
@@ -11,14 +11,14 @@ LEDCube::LEDCube() : Adafruit_GFX(8, 8)
 
 void LEDCube::begin(void)
 {
-	Adafruit_NeoPixel *pixels1 = new Adafruit_NeoPixel(NUMPIXELS, PIN1, NEO_GRB + NEO_KHZ800);
+	Adafruit_NeoPixel *pixels1 = new Adafruit_NeoPixel(NUMPIXELS, PIN1, NEO_GRB + NEO_KHZ800);  // pokazivac koji pokazuje na prvi layer
 	Adafruit_NeoPixel *pixels2 = new Adafruit_NeoPixel(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
 	Adafruit_NeoPixel *pixels3 = new Adafruit_NeoPixel(NUMPIXELS, PIN3, NEO_GRB + NEO_KHZ800);
 	Adafruit_NeoPixel *pixels4 = new Adafruit_NeoPixel(NUMPIXELS, PIN4, NEO_GRB + NEO_KHZ800);
 	Adafruit_NeoPixel *pixels5 = new Adafruit_NeoPixel(NUMPIXELS, PIN5, NEO_GRB + NEO_KHZ800);
 	Adafruit_NeoPixel *pixels6 = new Adafruit_NeoPixel(NUMPIXELS, PIN6, NEO_GRB + NEO_KHZ800);
 	Adafruit_NeoPixel *pixels7 = new Adafruit_NeoPixel(NUMPIXELS, PIN7, NEO_GRB + NEO_KHZ800);
-	Adafruit_NeoPixel *pixels8 = new Adafruit_NeoPixel(NUMPIXELS, PIN8, NEO_GRB + NEO_KHZ800);
+	Adafruit_NeoPixel *pixels8 = new Adafruit_NeoPixel(NUMPIXELS, PIN8, NEO_GRB + NEO_KHZ800); // pokazivac koji pokazuje na zadnji layer 
 	
 	pixels[0] = pixels1;
 	pixels[1] = pixels2;
@@ -29,13 +29,13 @@ void LEDCube::begin(void)
 	pixels[6] = pixels7;
 	pixels[7] = pixels8;
 	
-	for (int i = 0; i < LAYERS; i++)
+	for (int i = 0; i < LAYERS; i++) //petlja koja prolazi kroz polje unutar kojega se nalaze svi layeri
 	{
 		pixels[i]->begin();
 	}
 }
 
-void LEDCube::clear(void)
+void LEDCube::clear(void)  //funkcija za brisanje odnosno iskucivanje svih led dioda odnosno layera 
 {
 	for (int i = 0; i < LAYERS; i++)
 	{
@@ -46,7 +46,7 @@ void LEDCube::clear(void)
 	}
 }
 
-void LEDCube::display(void)
+void LEDCube::display(void) // funkcija za prikaz odnosno ukljucivanje led dioda
 {
 	for (int i = 0; i < LAYERS; i++)
 	{
@@ -54,7 +54,7 @@ void LEDCube::display(void)
 	}
 }
 
-void LEDCube::drawPixel(int16_t x, int16_t y, uint16_t color)
+void LEDCube::drawPixel(int16_t x, int16_t y, uint16_t color)  // funkcija za ukljucivanje odredenog pixela na odredenoj dubini unutar led kocke
 {
 	if ((x < 0) || (x > 7) || (y < 0) || (y > 7)) return;
 	int16_t _x0 = (_depth % 2 == 0 ? (7 - x) : x);
@@ -74,39 +74,12 @@ void LEDCube::drawPixel(int16_t x, int16_t y, uint16_t color)
 	//Serial.println();
 }
 
-void LEDCube::setDepth(int _d)
+void LEDCube::setDepth(int _d)  //podesavanje dubine odnosno z osi na led kocki 
 {
-	if ((_d > 7) || (_d < 0)) return;
+	if ((_d > 7) || (_d < 0)) return; 
 	_depth = _d;
 }
 
-void LEDCube :: drawPixel_2(int16_t a, int16_t b, uint16_t color)
-{
-	
-	if ((a < 0) || (a > 64) || (b < 0) || (b > 64)) return;
-	int16_t _x1 = (_depth2 % 2 == 0 ? (64 - a) : b);
-	pixels[b]->setPixelColor((_depth2 * 8) + _x1, color);
-}
 
-void LEDCube::setDepth_2(int _d2)
-{
-	if ((_d2 > 64) || (_d2 < 0)) return;
-	_depth2 = _d2;
-}
-
-
-void LEDCube :: drawPixel_3(int16_t x1, int16_t y1, uint16_t color)
-{
-	
-	if ((x1 < 0) || (x1 > 7 )|| (y1 < 0) || (y1 > 7)) return;
-	int16_t _x2 = (x1 % 2 == 0 ? (7 - _depth3) : y1);
-	pixels[y1]->setPixelColor((x1 * 8) + _x2, color);
-}
-
-void LEDCube::setDepth_3(int _d3)
-{
-	if ((_d3 > 8) || ( _d3< 0)) return;
-	_depth3 = _d3;
-}
 
 
